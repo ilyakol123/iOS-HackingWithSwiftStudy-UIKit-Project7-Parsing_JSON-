59820fb0ac73f99adeabdd5ba868e726d39711ff
Kolesnikov Ilya <ilja-kolesnikov@mail.ru>
2025-02-11 17:54:41 +0300

    Part 5 done Added "Top Rated" tab

---
+++ Project 7 (UIKit)/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project 7 (UIKit)
+//
+//  Created by Илья Колесников on 10.02.2025.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project 7 (UIKit)/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project 7 (UIKit)
+//
+//  Created by Илья Колесников on 10.02.2025.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
