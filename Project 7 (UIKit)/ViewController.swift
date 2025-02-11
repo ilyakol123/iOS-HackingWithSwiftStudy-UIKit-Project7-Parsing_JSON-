da828e265f44c2a2ab6544002ec2137336848b79
Kolesnikov Ilya <ilja-kolesnikov@mail.ru>
2025-02-11 19:53:34 +0300

    Wrap up for Project 7 done Added search button, credits and reset search button

--- Project 7 (UIKit)/ViewController.swift
+++ Project 7 (UIKit)/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [Petition]()
+    var originalPetitions: [Petition] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showCredits))
         let urlString: String
+        
+        //navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.search, target: self, action: #selector(findPetition))
+        
+        navigationItem.leftBarButtonItems = [ UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.refresh, target: self, action: #selector(resetSearch)),UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.search, target: self, action: #selector(findPetition)) ]
         
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -35,6 +41,7 @@ class ViewController: UITableViewController {
         let decoder = JSONDecoder()
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            originalPetitions = petitions
             tableView.reloadData()
         }
     }
@@ -61,7 +68,35 @@ class ViewController: UITableViewController {
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
-
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "This is using the public API", message: "We The People API of the Whitehouse.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func findPetition() {
+        let ac = UIAlertController(title: "Find a Petition", message: "Enter a keyword to search for a petition", preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Search", style: .default) { [weak self, weak ac] _ in
+            guard let searchingPetition = ac?.textFields?[0].text else { return }
+            self?.search(searchingPetition)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func search(_ keyword: String) {
+        let filteredPetitions = petitions.filter { $0.title.lowercased().contains(keyword.lowercased()) }
+        petitions = filteredPetitions
+        tableView.reloadData()
+    }
+    
+    @objc func resetSearch() {
+        petitions = originalPetitions
+        tableView.reloadData()
+    }
 
 }
 
